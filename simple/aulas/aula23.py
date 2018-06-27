4d057af21dcd3072fd9ed1e6c570c50147c00832
Vinicius Figueiredo <vinifysantos@gmail.com>
2018-06-26 22:15:01 -0300

    Add files via upload

---
+++ simple/aulas/aula23.py
@@ -0,0 +1,18 @@
+"""def func():
+    return 1, 2
+
+
+a, b = func()
+
+print(a)
+print(b)
+"""
+def potencia(x):
+    quadrado = x**3
+    cubo = x**4
+
+    return quadrado,cubo
+q,c = potencia(10)
+
+print(q)
+print(c)
