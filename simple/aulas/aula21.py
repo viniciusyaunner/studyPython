f70478d8a65aedfd1c3e7498ee2e900573e2111b
ViniciusFigueiredo <ViniciusFigueiredo@github.com>
2018-06-26 21:48:01 -0300

    only test about function

---
+++ simple/aulas/aula21.py
@@ -0,0 +1,8 @@
+def dados_pessoais(nome, sobrenome, idade, sexo):
+    print("nome: {}\nSobrenome: {}\nIdade: {}Sexo: ".format(nome,sobrenome,idade,sexo))
+#dados_pessoais("claudio","augusto",15,True)
+dados_pessoais(nome="claudio",
+               sobrenome="rogerio",
+               idade=30,
+               sexo=True)
+
